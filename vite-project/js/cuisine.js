bc44d3131d6ab438a3352dd6743daf01d41297de
amyl164 <amyl81008@gmail.com>
2023-11-13 10:12:47 -0500

    h

--- vite-project/js/cuisine.js
+++ vite-project/js/cuisine.js
@@ -1,11 +1,40 @@
 
 export{cuisine,price};
 
-const cuisine = [
+const category = [
     {
       name: "Cantonese Char Siu",
       price: "$",
       cuisine: "chinese",
     },
-  ]
+    {
+      name: "Cantonese Dim Sum",
+      price: "$$",
+      cuisine: "chinese",
+    },
+    {
+      name: "Beijing Roasted Duck",
+      price: "$$",
+      cuisine: "chinese",
+    },
+    {
+      name: "Sushi",
+      price: "$$$",
+      cuisine: "japanese",
+    },
+    {
+      name: "Taiyaki",
+      price: "$$",
+      cuisine: "japanese",
+    },
+    {
+      name: "Japaenese Curry",
+      price: "$$",
+      cuisine: "japanese",
+    },
+  ];
+
+/*   const printchinese = category.filter((category)=>category.cuisine ==="chinese").forEach((category)=> console.log(category.cuisine)); */
+
+
   
