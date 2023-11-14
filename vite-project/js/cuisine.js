1c95737c7c5881b8933459ef8cb6c11b02de01d2
amyl164 <amyl81008@gmail.com>
2023-11-14 10:13:47 -0500

    q

--- vite-project/js/cuisine.js
+++ vite-project/js/cuisine.js
@@ -1,40 +1,43 @@
 
-export{cuisine,price};
+/* export{cuisine,price}; */
 
 const category = [
     {
       name: "Cantonese Char Siu",
       price: "$",
       cuisine: "chinese",
+      pic: "https://takestwoeggs.com/wp-content/uploads/2021/10/Char-Siu-Pork-Homemade-Chinese-BBQ-Pork-Takestwoeggs-Final-SQ.jpg",
     },
     {
       name: "Cantonese Dim Sum",
       price: "$$",
       cuisine: "chinese",
+      pic: "https://www.dimsumcentral.com/wp-content/uploads/2018/06/dim-sum-menu-guide-header-new.jpg",
     },
     {
       name: "Beijing Roasted Duck",
       price: "$$",
       cuisine: "chinese",
+      pic: "https://redhousespice.com/wp-content/uploads/2022/01/sliced-peking-duck-with-pancakes-1-scaled.jpg",
     },
     {
       name: "Sushi",
       price: "$$$",
       cuisine: "japanese",
+      pic: "https://rimage.gnst.jp/livejapan.com/public/article/detail/a/00/00/a0000370/img/basic/a0000370_main.jpg?20201002142956",
     },
     {
       name: "Taiyaki",
       price: "$$",
       cuisine: "japanese",
+      pic: "https://www.justonecookbook.com/wp-content/uploads/2022/08/Japanese-Taiyaki-9851-1-500x500.jpg",
     },
     {
       name: "Japaenese Curry",
       price: "$$",
       cuisine: "japanese",
+      pic: "https://www.sbfoods-worldwide.com/recipes/on2e440000005dkl-img/010_JapaneseCurry_20230414_recipe.jpg",
     },
   ];
 
-/*   const printchinese = category.filter((category)=>category.cuisine ==="chinese").forEach((category)=> console.log(category.cuisine)); */
-
-
-  
+export {category};
--- vite-project/js/main.js
+++ vite-project/js/main.js
@@ -1,5 +1,8 @@
-import './style.css'
-import "../css/style.css"; 
+import "../css/style.css"
+import {category} from './cuisine'
 /* import {cuisine, price} from "./cuisine";
 console.log(cuisine, price) */
+
+
+const printchinese = category.filter((category)=>category.cuisine ==="chinese").forEach((category)=> console.log(category.cuisine)); 
 
